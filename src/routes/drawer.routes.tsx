21d6e90051c5fcb7f63a98a53e1644bf3c68fb20
Kauê Luí <ofckaue@gmail.com>
2024-04-08 22:20:34 -0300

    Stack Routes

--- src/routes/drawer.routes.tsx
+++ src/routes/drawer.routes.tsx
@@ -2,6 +2,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Feather } from '@expo/vector-icons';
 
 import TabRoutes from './tab.routes';
+import StackRoutes from './stack.routes';
 
 
 const Drawer = createDrawerNavigator();
@@ -10,6 +11,7 @@ export default function DrawerRoutes() {
     return (
         <Drawer.Navigator screenOptions={{ title: '' }}>
             <Drawer.Screen name="Home" component={TabRoutes} options={{ drawerIcon: ({ color, size }) => <Feather name="home" color={color} size={size} />, drawerLabel: 'Início' }} />
+            <Drawer.Screen name="Profile" component={StackRoutes} options={{ drawerIcon: ({ color, size }) => <Feather name="user" color={color} size={size} />, drawerLabel: 'Meu Perfil' }} />
         </Drawer.Navigator>
     );
 }
